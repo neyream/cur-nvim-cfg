f0210041f3d0faa6789fd7018d9e81bf955f1a0f
neyream <nareamsworld@gmail.com>
2025-06-16 13:15:39 +0300

    adding pair<int, int> and insert moves removing

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,11 +2,11 @@
 vim.g.mapleader = " "
 
 -- Бинды для перемещения в insert mode
-vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
-
+-- vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
+-- vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
+-- vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
+-- vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
+--
 -- бинд для автоматического закрытия интерфейса дебаггера
 vim.keymap.set("n", "<leader>ds", function()
 	require("dap").close()
